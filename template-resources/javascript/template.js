a8457b11d5ca987595a45329388449295aef0abb
Oliver Dain <oliver@dains.org>
2014-05-13 15:06:44 -0700

    The positioning and sizing of the dropdown menu is now done mostly with CSS rather than Javascript.

--- template-resources/javascript/template.js
+++ template-resources/javascript/template.js
@@ -2,12 +2,7 @@ var setup = function() {
     $('.dropdown-button').on('click', function(e) {
         var $list = $(e.currentTarget).children('.dropdown-list');
         var list = $list.get(0);
-        var parentRect = list.parentElement.getBoundingClientRect();
-        var top = parentRect.bottom + 10;
-        list.style.top = '' + top + 'px';
-        list.style.left = '' + parentRect.left + 'px';
         $list.toggleClass('visible-dropdown-list');
-        console.dir(e);
     });
 };
 
