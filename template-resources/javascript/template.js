9b6ce41f987c87764b5d1eb962faff0a48fb0e06
Oliver Dain <oliver@dains.org>
2014-05-13 15:18:11 -0700

    dropdown menu clicks now correctly hide previously visible items, etc.

--- template-resources/javascript/template.js
+++ template-resources/javascript/template.js
@@ -1,8 +1,13 @@
 var setup = function() {
     $('.dropdown-button').on('click', function(e) {
         var $list = $(e.currentTarget).children('.dropdown-list');
-        var list = $list.get(0);
-        $list.toggleClass('visible-dropdown-list');
+        if ($list.hasClass('visible-dropdown-list')) {
+          $list.removeClass('visible-dropdown-list');
+        } else {
+          // First hide whatever was visible before.
+          $('.visible-dropdown-list').removeClass('visible-dropdown-list');
+          $list.addClass('visible-dropdown-list');
+        }
     });
 };
 
