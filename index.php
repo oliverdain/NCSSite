2cba6120ae40a1ad177bf55cdd1d65c3f42ed9ca
Oliver Dain <oliver@dains.org>
2014-05-21 18:08:54 -0700

    We now render a 404 for any requested .html page that doesn't exist on the site.

--- index.php
+++ index.php
@@ -13,15 +13,38 @@ $twig = new Twig_Environment($loader, array(
 ));
 
 
-if ($_GET['page']) {
+function render_404($requested_page) {
+   header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found"); 
+   render_template('404.html', array('requested_page' => $requested_page));
+}
+
+// Sends the client a page based on the master.html template. This function 
+// should *not* be used for templates that do not inherit from master.html.
+// If extra_template_args is set they will be sent to the template along with 
+// the normal stuff master.html needs (the menu, the <link> tag for the 
+// generated CSS, etc.)
+function render_template($page, $extra_template_args = NULL) {
+   global $twig, $menu;
+   $css_tag = csscrush_tag('/resources/template.css', array('minify' => false));
+   $template_data = array('menu' => $menu, 'css_tag' => $css_tag);
+   if (isset($extra_template_args)) {
+      $template_data = array_merge($template_data, $extra_template_args);
+   }
+   $template = $twig->loadTemplate($page);
+   echo $template->render($template_data);
+}
+
+if (isset($_GET['page'])) {
    $page = $_GET['page'];
 } else {
    $page = 'index.html';
 }
 
-$template = $twig->loadTemplate($page);
-$css_tag = csscrush_tag('resources/template.css', array('minify' => false));
-echo $template->render(array('menu' => $menu, 'css_tag' => $css_tag));
+try {
+   render_template($page);
+} catch (Twig_Error_Loader $e) {
+   render_404($page);
+}
 
 ?>
 
--- menu.php
+++ menu.php
@@ -12,15 +12,15 @@ class MenuItem {
 
 $menu = array(
    'About NCS' => array(
-      new MenuItem('Introduction to NCS', 'intro.html'),
-      new MenuItem('Our Charter', 'charter.html'),
-      new MenuItem('Enrollment', 'enrollment.html')),
+      new MenuItem('Introduction to NCS', '/intro.html'),
+      new MenuItem('Our Charter', '/charter.html'),
+      new MenuItem('Enrollment', '/enrollment.html')),
    'Students and Families' => array(
-      new MenuItem('Calendar', 'calendar.html'),
-      new MenuItem('Classes', 'classes.html')),
+      new MenuItem('Calendar', '/calendar.html'),
+      new MenuItem('Classes', '/classes.html')),
    'Our Partners' => array(
-      new MenuItem('Le Petit Gourmet', 'lepetit.html'),
-      new MenuItem('MECCA', 'mecca.html'),
-      new MenuItem('Nearby Nature', 'nn.html'))
+      new MenuItem('Le Petit Gourmet', '/lepetit.html'),
+      new MenuItem('MECCA', '/mecca.html'),
+      new MenuItem('Nearby Nature', '/nn.html'))
    );
 ?>
